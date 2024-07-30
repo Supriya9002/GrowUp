fda05394471e7e2586f47bca161c36cec17676d9
Supriya Haldar <112514582+Supriya9002@users.noreply.github.com>
2024-07-31 01:51:57 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -63,7 +63,7 @@ app.use("/api/class",classRouter);
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'assets', 'index.html'));
   })
 
 app.use((err,req,res,next)=>{
